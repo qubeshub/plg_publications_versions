b3fe5bd6c04b63c94a444053cd555ca7ded3334f
M. Drew LaMar <mdlama@wm.edu>
2021-07-22 10:57:55 -0400

    [refactor] Pub plugins now using Qubeshub override
    (cherry picked from commit 5743a0cf6eb959eebe3aa572ef538307054e4e1b)

--- versions.php
+++ versions.php
@@ -8,10 +8,12 @@
 // No direct access
 defined('_HZEXEC_') or die();
 
+require_once PATH_APP . DS . 'libraries' . DS . 'Qubeshub' . DS . 'Plugin' . DS . 'Plugin.php';
+
 /**
  * Publications Plugin class for versions
  */
-class plgPublicationsVersions extends \Hubzero\Plugin\Plugin
+class plgPublicationsVersions extends \Qubeshub\Plugin\Plugin
 {
 	/**
 	 * Affects constructor behavior. If true, language files will be loaded automatically.
